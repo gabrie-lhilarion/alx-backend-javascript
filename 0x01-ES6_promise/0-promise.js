c0fdba7337e8d93d0ca5c17a4cc58ca2ecfb1864
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-06-27 01:18:35 +0100

    Keep every promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,10 @@
-function getResponseFromAPI(
-	
-)
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const response = { data: 'My sample data' };
+            resolve(response);
+        }, 1000);
+    });
+}
+
+export default getResponseFromAPI
