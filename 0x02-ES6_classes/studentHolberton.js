838e6378d70a067ba93a35fcd2a7f84ad0c73924
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-06-28 00:26:28 +0100

    hoisting lint

---
+++ 0x02-ES6_classes/studentHolberton.js
@@ -0,0 +1,22 @@
+
+import { HolbertonClass } from './holbertonClass.js';
+
+export class StudentHolberton {
+    constructor(firstName, lastName, holbertonClass) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._holbertonClass = holbertonClass;
+    }
+
+    get fullName() {
+        return `${this._firstName} ${this._lastName}`;
+    }
+
+    get holbertonClass() {
+        return this._holbertonClass;
+    }
+
+    get fullStudentDescription() {
+        return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    }
+}
