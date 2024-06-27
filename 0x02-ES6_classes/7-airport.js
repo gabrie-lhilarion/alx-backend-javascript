18d2ab2a95649a6affc4e0be98e9c592d22bf53b
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-06-27 23:50:32 +0100

    Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,40 +1,35 @@
-export default class HolbertonClass {
-  constructor(size, location) {
-    this.size = size;
-    this.location = location;
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
   }
 
-  // Getter for size
-  get size() {
-    return this._size;
+  // Getter for name
+  get name() {
+    return this._name;
   }
 
-  set size(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Size must be a number');
-    }
-    this._size = value;
-  }
-
-  // Getter for location
-  get location() {
-    return this._location;
-  }
-
-  set location(value) {
+  set name(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('Location must be a string');
+      throw new TypeError('Name must be a string');
     }
-    this._location = value;
+    this._name = value;
   }
 
-  // When cast to a number, return the size
-  [Symbol.toPrimitive](hint) {
-    if (hint === 'number') {
-      return this._size;
-    } if (hint === 'string') {
-      return this._location;
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
     }
-    return null;
+    this._code = value;
+  }
+
+  // Override the default string description
+  toString() {
+    return this._code;
   }
 }
