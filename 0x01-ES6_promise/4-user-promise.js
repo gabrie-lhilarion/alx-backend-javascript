1ff68abc5bb2689e4b7968f1e1c0d3604610eb5b
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-06-27 18:00:30 +0100

    User promise lint code

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,9 +1,8 @@
 function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-        firstName,
-        lastName,
-    });
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
 }
 
 export default signUpUser;
-
