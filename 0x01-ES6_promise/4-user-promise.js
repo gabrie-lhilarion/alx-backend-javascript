3e75c7152494de5740b77a97fc8777176354326e
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-06-27 17:58:41 +0100

    User promise add newline

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,9 @@
 function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName,
-    lastName,
-  });
+    return Promise.resolve({
+        firstName,
+        lastName,
+    });
 }
 
 export default signUpUser;
+
