380c93fc35cbcfae2713ba07657e8c8d381fef1b
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-07-03 03:27:19 +0100

    more Map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+function updateUniqueItems(groceries) {
+  if (!(groceries instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  groceries.forEach((quantity, item) => {
+    if (quantity === 1) {
+      groceries.set(item, 100);
+    }
+  });
+
+  return groceries;
+}
+
+export default updateUniqueItems;
